3bb3fceda6c5b87f8a2a8070164af4f7b58232fa
Oseias Lissak <oseiaslissak@hotmail.com>
2025-08-28 01:23:14 -0300

    Atualização Apis Projeto

--- assets/js/scrips.js
+++ assets/js/scrips.js
@@ -2,7 +2,7 @@
 const apiKeyWeather = "c0ff4e1edf5f6a03b52c78d36e7edc82";
 const apiKeyTimeZoneDb = "OF6CZ95QMSQJ"
 const apiCountryURL = "https://countryflagsapi.com/png/";
-const apiUnsplash = "https://source.unsplash.com/1600x900/?";
+const apiUnsplashKey = "GyrBDpBL0sgOOTSghSb7tgGcif-t7JRhM8fr1Bh0Vlg";
 const apiKeyIpGeolocation = "39dfe0024e624b7f902b4842d618d946";
 const apiKeyApiNinjas = "HH5+R9kHiPWwfD5ObdlaUw==gO377eLIHNbxaX3D";
 const apiKeyBigData = "bdc_a82aaa0389c04063a35f56a20aa81d61";
@@ -28,10 +28,7 @@ const languageElement = document.querySelector("#language span");
 const coinElement = document.querySelector("#coin span");
 const codeElement = document.querySelector("#code-calling span");
 
-
-
-
-//Funções
+// Funções
 const getWeatherData = async(city) => {
     const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKeyWeather}&lang=pt_br`;
 
@@ -39,31 +36,45 @@ const getWeatherData = async(city) => {
     const data = await res.json();
     console.log(data)
 
-    getPopulation(data.name), getTimeZoneDate(data.name), getCountry(data.sys.country);  
-    return(data);   
+    getPopulation(data.name), getTimeZoneDate(data.name), getCountry(data.sys.country);
+    return(data);
 }
 
+
+//Fuso horário
 const getTimeZoneDate = async (city) => {
-    
-    const apiTimeZoneDateURL = `https://api.api-ninjas.com/v1/worldtime?city=${city}`
-
+    const apiTimeZoneDateURL = `https://api.api-ninjas.com/v1/worldtime?city=${encodeURIComponent(city)}`;
     const initNinjas = {
-        headers: { 'X-Api-Key': 'HH5+R9kHiPWwfD5ObdlaUw==gO377eLIHNbxaX3D'},
-        contentType: 'application/json',
+        method: 'GET',
+        headers: { 'X-Api-Key': apiKeyApiNinjas },
+    };
+
+    try {
+        const response = await fetch(apiTimeZoneDateURL, initNinjas);
+        if (!response.ok) {
+            throw new Error(`Erro ao buscar fuso horário: ${response.status}`);
+        }
+        const data = await response.json();
+
+        if (data.hour !== undefined && data.minute !== undefined && data.second !== undefined) {
+            timezoneElement.innerText = `${data.hour}:${data.minute}:${data.second}`;
+            dateElement.innerText = `${data.day_of_week}, ${data.day}-${data.month}-${data.year}`;
+        } else {
+            timezoneElement.innerText = "N/A";
+            dateElement.innerText = "N/A";
+        }
+
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.error(error);
+        timezoneElement.innerText = "Erro";
+        dateElement.innerText = "Erro";
+        return null;
     }
-
-    const responseTimeZoneDate = await fetch(apiTimeZoneDateURL, initNinjas) ;
-    const apiTimeZoneDateData = await responseTimeZoneDate.json();
-
-    timezoneElement.innerText = `${apiTimeZoneDateData.hour}:${apiTimeZoneDateData.minute}:${apiTimeZoneDateData.second}`;
-    dateElement.innerText = `${apiTimeZoneDateData.day_of_week}, ${apiTimeZoneDateData.day}-${apiTimeZoneDateData.month}-${apiTimeZoneDateData.year}`;
-
-    console.log(apiTimeZoneDateData);
-
-    return(apiTimeZoneDateData);
 }
 
-
+//População
 const getPopulation = async (city) => {
     
     const apiPopulationURL = `https://api.api-ninjas.com/v1/city?name=${city}`
@@ -83,6 +94,7 @@ const getPopulation = async (city) => {
 
 }
 
+//País
 const getCountry = async (country) => {
 
     const apiBigDataURL = `https://api.bigdatacloud.net/data/country-info?code=${country}&key=${apiKeyBigData}`;
@@ -98,6 +110,27 @@ const getCountry = async (country) => {
     
 }
 
+//Imagem de fundo
+const getUnsplashImage = async(city) => {
+    const apiUnsplashURL = `https://api.unsplash.com/search/photos?query=${city}&client_id=${apiUnsplashKey}`;
+
+    try {
+        const res = await fetch(apiUnsplashURL);
+        const data = await res.json();
+        
+        if (data.results && data.results.length > 0) {
+            const randomIndex = Math.floor(Math.random() * data.results.length);
+            const imageUrl = data.results[randomIndex].urls.full;
+            return imageUrl;
+        } else {
+            return null; // Retorna null se não houver resultados
+        }
+    } catch (error) {
+        console.error("Erro ao buscar imagem do Unsplash:", error);
+        return null;
+    }
+};
+
 //ERROR
 const showErrorMessage = () => {
     errorMessageContainer.classList.remove("hide");
@@ -108,28 +141,32 @@ const showWeatherData = async (city) => {
     const data = await getWeatherData(city);
 
     console.log(data)
-   
-    if(data.cod === "404"){        
+    
+    if(data.cod === "404"){
         showErrorMessage();
         return;
     } else{
         errorMessageContainer.classList.add("hide");
         cityElement.innerText = data.name;
         tempElement.innerText = parseInt(data.main.temp);
-        countryElement.setAttribute("src", `https://countryflagsapi.com/png/${data.sys.country}`);
+        // Exemplo: tamanho de largura 160px
+        const countryCode = data.sys.country;
+        countryElement.setAttribute("src", `https://flagcdn.com/w160/${countryCode.toLowerCase()}.png`);
+        /* countryElement.setAttribute("src", `https://countryflagsapi.com/png/${data.sys.country}`); */
         descElement.innerText = data.weather[0].description;
         weatherIconElement.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
-        humidityElement.innerText = `${data.main.humidity}%`; 
+        humidityElement.innerText = `${data.main.humidity}%`;
         windElement.innerText = `${data.wind.speed}km/h`;
-
-        
         
         // Change bg image
-        document.body.style.backgroundImage = `url("${apiUnsplash + city}")`;
+        const imageUrl = await getUnsplashImage(city);
+        if (imageUrl) {
+            document.body.style.backgroundImage = `url("${imageUrl}")`;
+        }
+
         weatherDataContainer.classList.remove("hide");
     }
-
-}   
+}
 
 //Eventos
 
@@ -137,8 +174,8 @@ const showWeatherData = async (city) => {
 searchBtn.addEventListener("click", function (event){
     event.preventDefault();
     const city = cityInput.value;
-    showWeatherData(city), getTimeApi(city);
-}); 
+    showWeatherData(city);
+});
 
 cityInput.addEventListener("keyup", (e) =>{
     if(e.code === "Enter"){
